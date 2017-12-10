ee4290e3892b60c4262824eefffd27b925c86f45
William Book <book.wille@gmail.com>
2017-12-10 01:40:14 +0100

    Add lol2code.py

---
+++ lol2code.py
@@ -0,0 +1,12 @@
+import time
+
+print("----- Getting info.json")
+import lol2info
+
+print("----- Generating c# code")
+import info2cs
+
+print("----- Generating c++ code")
+import info2cpp
+
+sleep(1)
